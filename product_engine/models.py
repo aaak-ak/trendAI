07c0d8d10819fb9ff96b5c2f60c6691cb9b1634d
a_k <aaak_ak@yahoo.com>
2026-09-19 16:06:52 +0000

    product_id, aliexpress_id, ebay_id, etsy_id

--- product_engine/models.py
+++ product_engine/models.py
@@ -18,25 +18,28 @@ class Product(models.Model):
         ],
         default="Amazon"
     )
+
+    # окремі ID для кожної платформи
     asin = models.CharField(max_length=20, blank=True, null=True)       # Amazon ASIN
-    product_id = models.CharField(max_length=50, blank=True, null=True) # AliExpress/eBay/Etsy ID
-    affiliate_link = models.CharField(max_length=500, blank=True, null=True)             # партнерське посилання
+    aliexpress_id = models.CharField(max_length=50, blank=True, null=True)
+    ebay_id = models.CharField(max_length=50, blank=True, null=True)
+    etsy_id = models.CharField(max_length=50, blank=True, null=True)
+
+    affiliate_link = models.CharField(max_length=500, blank=True, null=True)  # партнерське посилання
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     likes = models.PositiveIntegerField(default=0)
 
     def save(self, *args, **kwargs):
-        # Amazon
-        if self.platform == "Amazon" and self.asin:
-            self.affiliate_link = f"https://www.amazon.de/dp/{self.asin}?tag=trendai-21"
-        # AliExpress
-        elif self.platform == "AliExpress" and self.product_id:
-            self.affiliate_link = f"https://www.aliexpress.com/item/{self.product_id}.html?aff_id=YOUR_ID"
-        # eBay
-        elif self.platform == "eBay" and self.product_id:
-            self.affiliate_link = f"https://www.ebay.com/itm/{self.product_id}?campid=YOUR_ID"
-        # Etsy
-        elif self.platform == "Etsy" and self.product_id:
-            self.affiliate_link = f"https://www.etsy.com/listing/{self.product_id}?ref=YOUR_ID"
+        # генеруємо affiliate_link тільки якщо воно пусте
+        if not self.affiliate_link:
+            if self.platform == "Amazon" and self.asin:
+                self.affiliate_link = f"https://www.amazon.de/dp/{self.asin}?tag=trendai-21"
+            elif self.platform == "AliExpress" and self.aliexpress_id:
+                self.affiliate_link = f"https://www.aliexpress.com/item/{self.aliexpress_id}.html?aff_id=YOUR_ID"
+            elif self.platform == "eBay" and self.ebay_id:
+                self.affiliate_link = f"https://www.ebay.com/itm/{self.ebay_id}?campid=YOUR_ID"
+            elif self.platform == "Etsy" and self.etsy_id:
+                self.affiliate_link = f"https://www.etsy.com/listing/{self.etsy_id}?ref=YOUR_ID"
 
         super().save(*args, **kwargs)
 
